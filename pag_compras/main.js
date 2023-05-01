1f9a178e9c4ed207224855511bc381b7574aaec6
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-05-01 16:34:15 -0500

    feat: :lipstick: Estilos botones
    en la seccion de compras se arreglaron pequeños estilos a los botones de compra, venta, y mostrar grafica

--- pag_compras/main.js
+++ pag_compras/main.js
@@ -1,7 +1,10 @@
 import myHeader from "../components/myHeader.js";
 import animation from "../storage/animation.js";
 import { showCompras } from "../components/funcionLogica.js";
-
+var select = document.getElementById("compras");
+select.addEventListener("click", function(){
+  this.classList.toggle("open");
+});
 animation.show()
 myHeader.show();
 showCompras;
